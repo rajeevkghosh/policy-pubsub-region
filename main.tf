6955f10d81659691169f839de489407d66253d42
rajeevkghosh <54052253+rajeevkghosh@users.noreply.github.com>
2021-10-01 18:28:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ provider "google" {
 resource "google_pubsub_topic" "example" {
   name = "example-topic"
 
-  message_storage_policy {
-    allowed_persistence_regions = [
-      "us-central1", "us-east-1", "us-west-1"
-    ]
-  }
+  #message_storage_policy {
+    #allowed_persistence_regions = [
+     # "us-central1", "us-east-1", "us-west-1"
+   # ]
+  #}
 }
