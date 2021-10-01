6ec772fb5e2992fd45de415720769e23d612c143
rajeevkghosh <54052253+rajeevkghosh@users.noreply.github.com>
2021-10-01 14:45:43 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "google" {
+ access_token = var.access_token
+}
+
+resource "google_pubsub_topic" "example" {
+  name = "example-topic"
+
+  message_storage_policy {
+    allowed_persistence_regions = [
+      "us-central1", "us-east-1", "us-west-1"
+    ]
+  }
+}
